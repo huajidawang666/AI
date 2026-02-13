b2cd456d34947c4d28c9a9ad3522514b56743f04
huajidawang666 <zhouyu25@mails.tsinghua.edu.cn>
2026-02-14 02:12:54 +0800

    Update image resize parameter to 256 in VGG model

--- CNN/VGG.py
+++ CNN/VGG.py
@@ -11,7 +11,7 @@ from torch.utils.data import DataLoader
 from torch import nn
 
 # Hyperparameters
-RESIZE=512
+RESIZE=256
 BATCH_SIZE = 64
 LEARNING_RATE = 0.2
 NUM_EPOCHS = 10
@@ -31,10 +31,10 @@ class VGG(nn.Module):
     def __init__(self):
         super().__init__()
         self.feature_extractor = nn.Sequential(
-            # Block 1: 2 convs, 64 filters
-            nn.Conv2d(1, 64, kernel_size=3, padding=1), nn.ReLU(inplace=True),
-            nn.Conv2d(64, 64, kernel_size=3, padding=1), nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2),
+            # # Block 1: 2 convs, 64 filters
+            # nn.Conv2d(1, 64, kernel_size=3, padding=1), nn.ReLU(inplace=True),
+            # nn.Conv2d(64, 64, kernel_size=3, padding=1), nn.ReLU(inplace=True),
+            # nn.MaxPool2d(kernel_size=2, stride=2),
 
             # Block 2: 2 convs, 128 filters
             nn.Conv2d(64, 128, kernel_size=3, padding=1), nn.ReLU(inplace=True),
