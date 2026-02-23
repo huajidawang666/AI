Fix gradient logging in training loop for improved clarity

CNN/NUNet.py
@@ -229,10 +229,10 @@ if __name__ == "__main__":
             # scaler.scale(loss).backward()
             
             # scaler.unscale_(optimizer)
-            # # print grad
-            # for name, param in model.named_parameters():
-            #     if param.grad is not None:
-            #         print(f"{name}: grad norm = {param.grad.norm().item():.4f}")
+            # print grad
+            for name, param in model.named_parameters():
+                if param.grad is not None:
+                    print(f"{name}: grad norm = {param.grad.norm().item():.4f}")
             torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
             
             loss.backward()