6cb6c40823b97a6f3a2c2e411dcc54c34f076318
huajidawang666 <zhouyu25@mails.tsinghua.edu.cn>
2026-02-23 10:18:59 +0800

    Adjust class weights in loss function for improved class balance during training

--- CNN/NUNet.py
+++ CNN/NUNet.py
@@ -195,7 +195,7 @@ if __name__ == "__main__":
     model.to(device)
     torch.compile(model)
     
-    weights = torch.tensor([5.0, 2.0, 1.0]).to(device)
+    weights = torch.tensor([2.5, 1.0, 1.0]).to(device)
     criterion_CE = nn.CrossEntropyLoss(weight=weights)
     criterion_Dice = DiceLoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=3e-4)
