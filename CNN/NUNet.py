ae942fa6058a83b6b1a98c493ba951398dbc0d85
huajidawang666 <zhouyu25@mails.tsinghua.edu.cn>
2026-02-23 09:39:27 +0800

    Remove gradient logging and unscale operations for cleaner training loop

--- CNN/NUNet.py
+++ CNN/NUNet.py
@@ -227,11 +227,11 @@ if __name__ == "__main__":
                     loss = loss_ce + loss_dice
 
             scaler.scale(loss).backward()
-            scaler.unscale_(optimizer)
-            # print grad
-            for name, param in model.named_parameters():
-                if param.grad is not None:
-                    print(f"{name}: grad norm = {param.grad.norm().item():.4f}")
+            # scaler.unscale_(optimizer)
+            # # print grad
+            # for name, param in model.named_parameters():
+            #     if param.grad is not None:
+            #         print(f"{name}: grad norm = {param.grad.norm().item():.4f}")
             torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
             scaler.step(optimizer)
             scaler.update()
